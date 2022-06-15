0b2ae65e65612edb26d3cf2b89468da6445823f8
Alain Sanguinetti <alain@sanguinetti.eu>
2022-06-15 09:25:08 +0200

    add fb check

--- conf/chatbot.py
+++ conf/chatbot.py
@@ -5,8 +5,12 @@
 import json
 from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters, RegexHandler,
 						  BaseFilter, ConversationHandler)
+import threading
+from facebook_scraper import get_posts
+
 from emoji import emojize
 import logging
+import shutil
 import config
 import certifi
 import urllib3
@@ -75,6 +79,42 @@ def main():
 	# SIGTERM or SIGABRT. This should be used most of the time, since
 	# start_polling() is non-blocking and will stop the bot gracefully.
 	updater.idle()
+	
+	check()
+	
+	
+#Define value for time between checks
+WAIT_SECONDS = 120
+
+#Scraping and composing of posts sent to Telegram Bot
+def check():
+    with open('fbpages.csv', mode='r+') as csv_file, NamedTemporaryFile(mode='w', delete=False) as tempfile:
+        csv_reader = csv.DictReader(csv_file)
+        csv_writer = csv.DictWriter(tempfile, fields)
+        line_count = 0
+        csv_writer.writeheader()
+        for page in csv_reader:
+            temp = None
+            posts = list(get_posts(page['fbpage_tag'], pages=2))
+            posts.sort(key = lambda x: x['time'])
+            for post in posts:
+                if post['time'] <= datetime.strptime(page['last_post_date'], date_format): # post already sent to channel
+                    break
+                if post['image'] is not None:
+                    bot.send_photo(chat_id, post['image'], (post['text'] if post['text'] else '')+ '\n[' + page['telegram_name'] + ']')
+                    if (post['time'] > datetime.strptime(page['last_post_date'], date_format) and temp is None):
+                        temp = post['time']
+                elif post['text'] is not None:
+                    bot.send_message(chat_id, (post['text'] if post['text'] else '')+ '\n[' + page['telegram_name'] + ']')
+                    if (post['time'] > datetime.strptime(page['last_post_date'], date_format) and temp is None):
+                        temp = post['time']
+            if temp is not None:
+                page['last_post_date'] = temp
+            row = {'telegram_name': page['telegram_name'], 'fbpage_tag': page['fbpage_tag'], 'last_post_date': page['last_post_date']}
+            csv_writer.writerow(row)
+        shutil.move(tempfile.name, 'fbpages.csv')
+        threading.Timer(WAIT_SECONDS, check).start()
+
 
 if __name__ == '__main__':
 	main()
